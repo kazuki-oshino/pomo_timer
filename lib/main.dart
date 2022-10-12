37e5829161120bb8ea622eb2267c5d02d34971cf
kazuki-oshino <kazuki.oshino@showcase-gig.com>
2022-10-12 10:22:53 +0900

    add system tray

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,22 @@
 import 'dart:async';
 
+import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:system_tray/system_tray.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
+  doWhenWindowReady(() {
+    final win = appWindow;
+    const initialSize = Size(900, 650);
+    win.minSize = initialSize;
+    win.maxSize = initialSize;
+    win.alignment = Alignment.center;
+    win.title = 'my timer';
+    win.show();
+  });
 }
 
 class MyApp extends StatelessWidget {
@@ -43,11 +55,22 @@ class _MyHomePageState extends State<MyHomePage> {
   static const int timerDefaultTime = 25 * 60;
   static const String completeCountKey = 'completeCount';
 
+  final AppWindow _appWindow = AppWindow();
+  final SystemTray _systemTray = SystemTray();
+  final Menu _menuMain = Menu();
+
   @override
   void initState() {
     super.initState();
 
     init();
+    initSystemTray();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _timer?.cancel();
   }
 
   void init() async {
@@ -60,6 +83,21 @@ class _MyHomePageState extends State<MyHomePage> {
     final completeCount = prefs.getInt(completeCountKey) ?? 0;
     _currentCompleteCount = completeCount;
     setState(() {});
+  }
+
+  Future<void> initSystemTray() async {
+    await _systemTray.initSystemTray(iconPath: 'assets/watanabe.jpeg');
+    _systemTray.setTitle('system tray!');
+    _systemTray.setToolTip('toolTipだよ！');
+
+    _systemTray.registerSystemTrayEventHandler((eventName) {
+      debugPrint('eventName: $eventName}');
+      if (eventName == kSystemTrayEventClick) {
+        _systemTray.popUpContextMenu();
+      } else if (eventName == kSystemTrayEventRightClick) {
+        _appWindow.show();
+      }
+    });
   }
 
   void startTimer() async {
@@ -80,6 +118,13 @@ class _MyHomePageState extends State<MyHomePage> {
           setState(() {
             _currentSeconds = _currentSeconds - 1;
           });
+          _systemTray.setTitle((int leftSeconds) {
+            final minutes =
+                (leftSeconds / 60).floor().toString().padLeft(2, '0');
+            final seconds =
+                (leftSeconds % 60).floor().toString().padLeft(2, '0');
+            return '$minutes:$seconds';
+          }(_currentSeconds));
         }
       },
     );
@@ -119,11 +164,6 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
